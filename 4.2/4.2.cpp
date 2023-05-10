3f4c5f89c2ae4efb217cb914f7f835cd2d2d47cf
Сергей Старовойтов <starovoytovsergeya@gmail.com>
2023-05-10 20:01:57 +0300

    homework-4

---
+++ 4.2/4.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "list_class.h"
+
+int main() {
+
+	List list;
+	list.PushBack(1);
+	list.PushBack(55);
+	list.PushFront(23);
+	std::cout << list.Size() << "\n";
+	std::cout << list.PopBack() << "\n";
+
+    return 0;
+}

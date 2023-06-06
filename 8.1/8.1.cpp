960f133bb7d859bf834ddeef044f918588bc30cd
Сергей Старовойтов <starovoytovsergeya@gmail.com>
2023-06-06 14:18:17 +0300

    homeworks 8

---
+++ 8.1/8.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+//[IN]: 1 1 2 5 6 1 2 4
+//[OUT] : 1 2 4 5 6
+int main()
+{
+    std::vector<int> cont =  { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "[IN] ";
+    for (const int& el : cont) {
+        std::cout << el << " ";
+    };
+    
+    std::cout << std::endl;
+
+    std::sort(cont.begin(), cont.end());
+    auto last_itr = std::unique(cont.begin(), cont.end());
+
+    cont.erase(last_itr, cont.end());
+
+    std::cout << "[OUT] ";
+    for (const int& el : cont) {
+        std::cout << el << " ";
+    };
+}
---
+++ 8.2/8.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+
+template<class Iterator> void printContainer(Iterator begin, Iterator end) {
+
+    while (begin != end) {
+
+        std::cout << *begin << " ";
+        begin++;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    printContainer(test_set.begin(), test_set.end()); // four one three two. помните почему такой порядок? :) 
+    // из-за лексико-графической сортировки set
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    printContainer(test_list.begin(), test_list.end()); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    printContainer(test_vector.begin(), test_vector.end()); // one, two, three, four
+}
